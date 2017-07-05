0fcb0441ffe902de4499d3de0038096d1e98e407
Loris Witschard <loris.wit@gmail.com>
2017-07-06 00:03:19 +0200

    Add cmder config

---
+++ cmder/user-aliases.cmd
@@ -0,0 +1,35 @@
+;= @echo off
+;= rem Call DOSKEY and use this file as the macrofile
+;= %SystemRoot%\system32\doskey /listsize=1000 /macrofile=%0%
+;= rem In batch mode, jump to the end of the file
+;= goto:eof
+;= Add aliases below here
+e.=explorer .
+
+gl=git log --oneline --all --graph --decorate $*
+gs=git status -s $*
+gd=git diff *$**
+gc=git commit -m "$*"
+gca=git commit -am "$*"
+gp=git push $*
+ga=git add *$**
+
+pwd=cd
+clear=cls
+history=cat %CMDER_ROOT%\config\.history
+unalias=alias /d $1
+vi=vim $*
+cmderr=cd /d "%CMDER_ROOT%"
+ls=ls --show-control-chars --color -F $*
+ll=ls --show-control-chars --group-directories-first --color -Flhv $*
+la=ls --show-control-chars --group-directories-first --color -FlAhv $*
+
+select=mysql -u root -e "select $*"
+show=mysql -u root -e "show $*"
+drop=mysql -u root -e "drop $*"
+update=mysql -u root -e "update $*"
+create=mysql -u root -e "create $*"
+insert=mysql -u root -e "insert $*"
+delete=mysql -u root -e "delete $*"
+
+config=atom %CMDER_ROOT%\config\user-aliases.cmd
